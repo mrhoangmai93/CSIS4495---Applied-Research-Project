6f1c44cb83adadf511096876d1348865efcb6a2b
ericdao37 <35124995+ericdao37@users.noreply.github.com>
2019-09-15 13:30:46 -0700

    add eslint package.json

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './client/App';
-import './client/scss/bootstrap.scss';
-import * as serviceWorker from './client/serviceWorker';
-import {BrowserRouter} from "react-router-dom";
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import rootReducer from './client/redux/reducers';
-import rootSaga from './client/redux/sagas';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./client/App";
+import "./client/scss/bootstrap.scss";
+//import * as serviceWorker from './client/serviceWorker';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import rootReducer from "./client/redux/reducers";
+import rootSaga from "./client/redux/sagas";
 
 // Redux & Saga Init
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(<Provider store={store}>
-    <App/>
-</Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
